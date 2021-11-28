c22178006868900575bb54cdcab7bbf1e3a60a9e
Chérie Oduwole <bellapette0@gmail.com>
2021-11-28 07:10:24 +0000

    Add the route /u/:shortURL that sends users to the long url of the short url, when the short url link is clicked or when they go to the short url link

--- express_server.js
+++ express_server.js
@@ -49,6 +49,11 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body["longURL"];
